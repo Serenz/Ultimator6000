ba0e280360e575512491bb14478bdc4348c786a0
Marco <marco.serena@external.a2a.eu>
2022-06-21 12:49:25 +0200

    Added cross OS support

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from tkinter import *
 from tkinter import filedialog
 import shutil
+from pathlib import Path
 
 
 root = Tk()
@@ -28,38 +29,40 @@ c1 = Checkbutton(root, text='Copiare in nuova cartella',variable=var1, onvalue=1
 c1.pack()
 
 def foo():
-    path = e.get()
+    path = Path(e.get())
     suffisso = div.get()
     dir_list = os.listdir(path)
 
     for sub_dir in dir_list:
         if sub_dir != 'COPIONE':
-            sub_path = f'{path}\\{sub_dir}'
+            sub_path = path / sub_dir
             sub_list = os.listdir(sub_path)
             os.chdir(sub_path)
             count = 1
             for f in sub_list:
-                file_path = f'{sub_path}\\{f}'
-                print('file path:', file_path)
+                file_path = sub_path / f
+                # print('file path:', file_path)
                 extension = f.split('.')[-1]
                 new_name = f'{sub_dir}-{count}-{suffisso}.{extension}'
                 os.rename(file_path, new_name)
                 count += 1
 
+    print('\nRenaming effettuato con successo!')
     if var1.get() == 1:
-        new_dir = f'{path}\\COPIONE'
+        new_dir = path / 'COPIONE'
         if not os.path.exists(new_dir):
             os.mkdir(new_dir)
 
-        print(dir_list)
+        # print(dir_list)
         for sub_dir in dir_list:
-            print(sub_dir)
+            # print(sub_dir)
             if sub_dir != 'COPIONE':
-                sub_path = f'{path}\\{sub_dir}'
-                print('\n\n')
-                print('sub_path', sub_path)
-                print('new_path', new_dir)
+                sub_path = path / sub_dir
+                # print('\n\n')
+                # print('sub_path', sub_path)
+                # print('new_path', new_dir)
                 shutil.copytree(sub_path, new_dir, dirs_exist_ok=True)
+        print('\nCopia effettuata con successo!')
 
 Button(root, text="Esegui", padx=20, pady=4, command=foo).pack()
 
